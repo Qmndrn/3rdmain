838dc3dc7ecf967a8749f2576f239f11d4831016
Qmndrn <ktoto.ruu@gmail.com>
2025-11-17 18:16:13 +0300

    Refactor authorization function for clarity

--- authorization.py
+++ authorization.py
@@ -1,31 +1,29 @@
 from cryptography.fernet import Fernet
 from main import load_key
 
-def authorization(f):
+
+def authorization(fernet: Fernet):
     login = input("Введите логин: ").strip()
     password = input("Введите пароль: ").strip()
 
+
     with open("passwords.txt", "r") as file:
         for line in file:
-            log, token = line.split("/", 1)
-            trash, stored_log = log.split(":", 1)
+            stored_log, token = line.split("/", 1)[0].split(":", 1)[1], line.split("/", 1)[1]
             if stored_log == login:
-                decrypted_pass = f.decrypt(token.encode()).decode()
+                decrypted_pass = fernet.decrypt(token.strip().encode()).decode()
                 if password == decrypted_pass:
-                    print("Вы успешно авторизованы")
-                    return True
-                else:
-                    print("Неверный пароль")
-                    return False
-        print("В базе нет такого пользователя")
-        return False
+                 print("Вы успешно авторизованы")
+                 return True
+            print("Неверный пароль")
+            return False
+    print("В базе нет такого пользователя")
+    return False
 
-def main():
+
+if __name__ == "__main__":
     key = load_key()
     f = Fernet(key)
     while True:
         if authorization(f):
             break
-
-if __name__ == "__main__":
-    main()
